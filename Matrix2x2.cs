98d0472879ade9428f0cb93b97ed0f8d2fbfdd64
Paweł Górski <karias0672@gmail.com>
2025-04-10 01:58:43 +0200

    Add project files.

---
+++ Matrix2x2.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace Fibonacci
+{
+    internal class Matrix2x2
+    {
+        //  |a, b|
+        //  |c, d|
+        public double a {get; set;}
+        public double b {get; set;}
+        public double c {get; set;}
+        public double d {get; set;}
+
+        public Matrix2x2(double a, double b, double c, double d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+
+        public static Matrix2x2 MatrixMultiply(Matrix2x2 MatrixA, Matrix2x2 MatrixB)
+        {
+            Matrix2x2 result = new(0,0,0,0);
+
+            result.a = (MatrixA.a * MatrixB.a) + (MatrixA.b * MatrixB.c);
+            result.b = (MatrixA.a * MatrixB.b) + (MatrixA.b * MatrixB.d);
+            result.c = (MatrixA.c * MatrixB.a) + (MatrixA.d * MatrixB.c);
+            result.d = (MatrixA.c * MatrixB.b) + (MatrixA.d * MatrixB.d);
+            return result;
+        }
+
+        public string ShowMatrix()
+        {
+            return $"|{a}, {b}|\n|{c}, {d}|";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Numerics;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fibonacci(1476) is the biggest calculable argument
+            
+            BigInteger bigInteger = new(GetFibonacci(1476));
+            Console.WriteLine(bigInteger);
+        }
+
+        public static double GetFibonacci(int fibonacciNumber)
+        {
+            Matrix2x2 fibMatrix = new(1, 1, 1, 0);
+            Matrix2x2 result = new(1, 1, 1, 0);
+            if(fibonacciNumber == 1 ||  fibonacciNumber == 2)
+            {
+                return 1;
+            }
+            for(int i = 0; i < fibonacciNumber - 2; i++)
+            {
+                result = Matrix2x2.MatrixMultiply(result, fibMatrix);
+            }
+            return result.a;
+            
+         }
+
+        public static void ShowFibonacci(int fibNumber)
+        {
+            string FibResult = $"Fibonacci of {fibNumber} = ";
+            Console.WriteLine(FibResult + GetFibonacci(fibNumber).ToString());
+        }
+    }
+
+
+}
